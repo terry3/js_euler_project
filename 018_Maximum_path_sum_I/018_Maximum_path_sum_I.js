a3b818b6fdc433c42514f96561301de4c157a6e8
Tengfei Guo <terryr3rd@yeah.net>
2016-05-14 21:38:42 +0800

    Add 018, 067.

---
+++ 018_Maximum_path_sum_I/018_Maximum_path_sum_I.js
@@ -0,0 +1,77 @@
+/*
+ By starting at the top of the triangle below and moving to adjacent
+ numbers on the row below, the maximum total from top to bottom is 23.
+3
+7 4
+2 4 6
+8 5 9 3
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem
+ by trying every route. However, Problem 67, is the same challenge with
+ a triangle containing one-hundred rows; it cannot be solved by brute force,
+ and requires a clever method! ;o)
+ */
+
+var origin = [];
+origin.push('75'.split(' '));
+origin.push('95 64'.split(' '));
+origin.push('17 47 82'.split(' '));
+origin.push('18 35 87 10'.split(' '));
+origin.push('20 04 82 47 65'.split(' '));
+origin.push('19 01 23 75 03 34'.split(' '));
+origin.push('88 02 77 73 07 63 67'.split(' '));
+origin.push('99 65 04 28 06 16 70 92'.split(' '));
+origin.push('41 41 26 56 83 40 80 70 33'.split(' '));
+origin.push('41 48 72 33 47 32 37 16 94 29'.split(' '));
+origin.push('53 71 44 65 25 43 91 52 97 51 14'.split(' '));
+origin.push('70 11 33 28 77 73 17 78 39 68 17 57'.split(' '));
+origin.push('91 71 52 38 17 14 91 43 58 50 27 29 48'.split(' '));
+origin.push('63 66 04 68 89 53 67 30 73 16 69 87 40 31'.split(' '));
+origin.push('04 62 98 27 23 09 70 98 73 93 38 53 60 04 23'.split(' '));
+
+var max = 0;
+var tmpArr = [];
+
+for (var i = 0; i < origin.length; i++) {
+  var array = [];
+  for (var j = 0; j < origin[i].length; j++) {
+    var first,
+        second;
+    if (i - 1 < 0) {
+      array[0] = parseInt(origin[i][j]);
+      break;
+    }
+    if (j === 0) {
+      array[j] = parseInt(origin[i][j]) + tmpArr[j];
+    } else if (j === origin[i].length - 1) {
+      array[j] = parseInt(origin[i][j]) + tmpArr[j - 1];
+    } else {
+      first = parseInt(origin[i][j]) + tmpArr[j - 1];
+      second = parseInt(origin[i][j]) + tmpArr[j];
+      array[j] = Math.max(first, second);
+    }
+  }
+  tmpArr = array;
+}
+
+console.log(tmpArr);
+console.log(Math.max.apply(Math, tmpArr));
---
+++ 067_Maximum_path_sum_II/067_Maximum_path_sum_II.js
@@ -0,0 +1,59 @@
+/*
+ By starting at the top of the triangle below and moving to adjacent
+ numbers on the row below, the maximum total from top to bottom is 23.
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom in triangle.txt (right click
+ and 'Save Link/Target As...'), a 15K text file containing a triangle
+ with one-hundred rows.
+
+NOTE: This is a much more difficult version of Problem 18. It is not
+ possible to try every route to solve this problem, as there are 299
+ altogether! If you could check one trillion (1012) routes every second it
+ would take over twenty billion years to check them all. There is an
+ efficient algorithm to solve it. ;o)
+ */
+
+var fs = require("fs");
+var data = fs.readFileSync('p067_triangle.txt');
+var tmpOrigin = data.toString().split('\n');
+var origin = [];
+for (var i = 0, j = 0; i < tmpOrigin.length; i++) {
+  if (tmpOrigin[i] === '') {
+    continue;
+  }
+  origin[j] = tmpOrigin[i].split(' ');
+  j++;
+}
+
+var max = 0;
+var tmpArr = [];
+
+for (var i = 0; i < origin.length; i++) {
+  var array = [];
+  for (var j = 0; j < origin[i].length; j++) {
+    var first,
+        second;
+    if (i - 1 < 0) {
+      array[0] = parseInt(origin[i][j]);
+      break;
+    }
+    if (j === 0) {
+      array[j] = parseInt(origin[i][j]) + tmpArr[j];
+    } else if (j === origin[i].length - 1) {
+      array[j] = parseInt(origin[i][j]) + tmpArr[j - 1];
+    } else {
+      first = parseInt(origin[i][j]) + tmpArr[j - 1];
+      second = parseInt(origin[i][j]) + tmpArr[j];
+      array[j] = Math.max(first, second);
+    }
+  }
+  tmpArr = array;
+}
+
+console.log(Math.max.apply(Math, tmpArr));
